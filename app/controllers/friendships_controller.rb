d42cf15c37a7d77eaf95faf7afeb785c5dbac73f
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-12-11 17:01:36 +0200

    add the create action for friendships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,4 +3,17 @@ class FriendshipsController < ApplicationController
     @requests = current_user.friendships.all
     @invitations = current_user.invitations.all
   end
+  
+  def create
+    @friendship = current_user.friendships.new(friendship_params)
+    if @friendship.save
+      redirect_to users_path, notice: "Friendship request sent successfully"
+    end
+  end
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:receiver_id, :status)
+  end
 end
