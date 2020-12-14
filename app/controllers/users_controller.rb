a836c1a644d9ddc24eb7c9b0cc79c8018e91a9c0
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-12-14 18:57:42 +0200

    add a send invite link to the single user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @friendship = Friendship.new
     @posts = @user.posts.ordered_by_most_recent
   end
 end
