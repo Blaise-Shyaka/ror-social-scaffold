2c66bd01ba3c9c63ac9df0b7e2e9e5314e2fade3
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-12-16 00:41:28 +0200

    remove logic from all users views

---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,29 @@
+module UserHelper
+  def display_send_invite_form
+    "<%= form_for @friendship, url: friendships_path, method: :post do |f| %>
+      <%= f.hidden_field :receiver_id, value: user.id %>
+      <%= f.hidden_field :status, value: nil %>
+      <%= f.submit 'Send Invite' %>
+    <% end %>"
+  end
+
+  def display_status_or_call_to_action_links(user)
+    invite = current_user.invitations.where(sender_id: user.id)
+    friendship = current_user.friendships.where(receiver_id: user.id)
+    markup = ''
+    if invite.any?
+      if invite[0].status
+        markup << 'You are friends'
+      elsif invite[0].status.nil?
+        return render 'handle_request', id: user.id
+      end
+    elsif friendship.any?
+      if friendship[0].status
+        markup << 'You are friends'
+      end
+    elsif friendship.none?
+      return render 'send_invite', user: user
+    end
+    markup.html_safe
+  end
+end
