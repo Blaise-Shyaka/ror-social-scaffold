f25cde6211ec60dd06301055e7c8801f60d4a4f8
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-12-17 00:21:41 +0200

    fix rubocop error

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -7,6 +7,7 @@ module UserHelper
     <% end %>"
   end
 
+  # rubocop:disable Metrics/CyclomaticComplexity
   def display_status_or_call_to_action_links(user)
     invite = current_user.invitations.where(sender_id: user.id)
     friendship = current_user.friendships.where(receiver_id: user.id)
@@ -24,6 +25,7 @@ module UserHelper
     end
     markup.html_safe
   end
+  # rubocop:enable Metrics/CyclomaticComplexity
 
   def display_each_user(user)
     if user == current_user
